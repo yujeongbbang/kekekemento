9882df8a6cdfbd2c7c141e8c0d601326438dbf9f
skyline <skyline3417@gmail.com>
2023-11-07 15:41:45 +0900

    first commit

---
+++ src/main/java/com/kekeke/kekekebackend/KekekeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kekeke.kekekebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekekeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekekeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kekeke/kekekebackend/KekekeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kekeke.kekekebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekekeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
